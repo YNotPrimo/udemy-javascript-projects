66220a127e4dd4526af3b493fd94b46b05a43251
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-07 19:51:15 +0300

    Learning events

--- 05-Events/assets/scripts/events.js
+++ 05-Events/assets/scripts/events.js
@@ -1,7 +1,62 @@
 const button = document.querySelector("button");
 
-const buttonClickHandler = () => {
-    alert("Button was clicked!");
-}
+const buttonClickHandler = event => {
+	// event.target.disabled = true;
+	console.log(event);
+};
 
-button.onclick = buttonClickHandler;
+const anotherButtonClickHandler = () => {
+	console.log("Clicked");
+};
+
+const boundFn = buttonClickHandler.bind(this);
+
+// button.addEventListener("click", boundFn);
+
+// setTimeout(() => {
+// 	button.removeEventListener("click", boundFn);
+// }, 2000);
+
+// buttons.forEach(btn => {
+// 	btn.addEventListener("mouseenter", buttonClickHandler);
+// })
+
+// window.addEventListener("")
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", event => {
+	event.preventDefault();
+	console.log(event);
+});
+
+const div = document.querySelector("div");
+
+div.addEventListener(
+	"mouseenter",
+	event => {
+		console.log("Clicked div");
+		console.log(event);
+	},
+	true
+);
+
+button.addEventListener("mouseenter", event => {
+	event.stopPropagation();
+	console.log("Clicked button");
+	console.log(event);
+});
+
+const listItems = document.querySelectorAll("li");
+const list = document.querySelector("ul");
+
+// listItems.forEach(item => {
+// 	item.addEventListener("click", event => {
+// 		event.target.classList.toggle("highligh");
+// 	});
+// });
+
+list.addEventListener("click", event => {
+	// event.target
+	event.target.closest("li").classList.toggle("highlight");;
+});
