0a1c8e4faf292ea67d2aa2d8d5745ffe6737925d
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-12 12:29:41 +0300

    Async/Await

--- 07-Async/app.js
+++ 07-Async/app.js
@@ -5,9 +5,11 @@ const getPosition = opts => {
 	const promise = new Promise((resolve, reject) => {
 		navigator.geolocation.getCurrentPosition(
 			success => {
-        resolve(success);
-      },
-			error => {},
+				resolve(success);
+			},
+			error => {
+				reject(error);
+			},
 			opts
 		);
 	});
@@ -23,20 +25,19 @@ const setTimer = duration => {
 	return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-  .then(posData => {
-    positionData = posData;
-    return setTimer(2000);
-  })
-  .then(data => {
-    console.log(data);
-  })
-	setTimer(1000).then(() => {
-		console.log("Timer done!");
-	});
-	console.log("Getting position...");
+async function trackUserHandler() {
+	let posData;
+  let timerData;
+  
+	try {
+		posData = await getPosition();
+    timerData = await setTimer(2000);
+    
+	} catch (error) {
+		console.log(error);
+	}
+
+	console.log(posData, timerData);
 }
 
 button.addEventListener("click", trackUserHandler);
