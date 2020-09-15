1e8b890b25992b330175cce530aa96ed0bc061e0
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-15 11:45:43 +0300

    EJS Template Learning

--- 09-Node/app.js
+++ 09-Node/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     res.setHeader("Content-Type", "text/html");
@@ -8,7 +15,10 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.send("<h1>Hello World!</h1>");
-})
+    const userName = req.body.username || "Unknown User";
+    res.render("index", {
+        userName: userName
+    });
+});
 
 app.listen(3000);
