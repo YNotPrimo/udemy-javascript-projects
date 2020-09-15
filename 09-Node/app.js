41c8e64985f3a453bb38d51982e34c52b8340f6d
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-15 11:51:07 +0300

    Refactored a line

--- 09-Node/app.js
+++ 09-Node/app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     const userName = req.body.username || "Unknown User";
     res.render("index", {
-        userName: userName
+        userName
     });
 });
 
