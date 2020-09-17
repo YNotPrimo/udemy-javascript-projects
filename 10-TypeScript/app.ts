041d1b77148c34734591efea0d0601c6ead26497
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-17 16:16:48 +0300

    Classes and Interfaces Learning

--- 10-TypeScript/app.ts
+++ 10-TypeScript/app.ts
@@ -1,5 +1,32 @@
+interface Greetable {
+	name: string;
+}
+
+interface Printable {
+	print(): void;
+}
+
+class User implements Greetable, Printable {
+	// name: string;
+	// private age: number;
+
+	constructor(public name: string, private age: number) {}
+	
+	print(): void {
+		console.log(this.name);
+		
+	}
+}
+
+class Admin extends User {
+    constructor(name: string, age: number, private permissions: string[]) {
+        super(name, age);
+    }
+}
 
 
+const user = new User("Primo", 30);
+console.log(user.name);
 
 const num1Input = document.getElementById("num1") as HTMLInputElement;
 const num2Input = document.getElementById("num2") as HTMLInputElement;
@@ -9,15 +36,18 @@ type PrintMode = "console" | "alert";
 type ResultType = string | number;
 
 enum OutputMode {
-    CONSOLE, ALERT
-};
+	CONSOLE,
+	ALERT,
+}
+
+
 
 function printResult(result: ResultType, printMode: OutputMode) {
-    if (printMode === OutputMode.CONSOLE){
-        console.log(result);
-    } else {
-        alert(result);
-    }
+	if (printMode === OutputMode.CONSOLE) {
+		console.log(result);
+	} else {
+		alert(result);
+	}
 }
 
 function add(a: number, b: number): number {
@@ -33,14 +63,14 @@ buttonElement.addEventListener("click", () => {
 	const result = add(num1, num2);
 
 	const resultContainer = {
-        res: result,
-        print() {
-            console.log(this.res);
-        }
+		res: result,
+		print() {
+			console.log(this.res);
+		},
 	};
 
 	results.push(resultContainer);
-    printResult(result, OutputMode.CONSOLE);
-    printResult(result, OutputMode.ALERT);
+	printResult(result, OutputMode.CONSOLE);
+	printResult(result, OutputMode.ALERT);
 	// console.log(results);
 });
