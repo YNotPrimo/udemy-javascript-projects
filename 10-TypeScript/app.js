8fb1f7f0ba5c421181edd1a1108d3afdd36ce3b5
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-17 15:52:09 +0300

    Enum and Literal Type Learning

--- 10-TypeScript/app.js
+++ 10-TypeScript/app.js
@@ -1,12 +1,36 @@
 var num1Input = document.getElementById("num1");
 var num2Input = document.getElementById("num2");
 var buttonElement = document.getElementById("button");
+var OutputMode;
+(function (OutputMode) {
+    OutputMode[OutputMode["CONSOLE"] = 0] = "CONSOLE";
+    OutputMode[OutputMode["ALERT"] = 1] = "ALERT";
+})(OutputMode || (OutputMode = {}));
+;
+function printResult(result, printMode) {
+    if (printMode === OutputMode.CONSOLE) {
+        console.log(result);
+    }
+    else {
+        alert(result);
+    }
+}
 function add(a, b) {
     return a + b;
 }
+var results = [];
 buttonElement.addEventListener("click", function () {
     var num1 = +num1Input.value;
     var num2 = +num2Input.value;
     var result = add(num1, num2);
-    console.log(result);
+    var resultContainer = {
+        res: result,
+        print: function () {
+            console.log(this.res);
+        }
+    };
+    results.push(resultContainer);
+    printResult(result, OutputMode.CONSOLE);
+    printResult(result, OutputMode.ALERT);
+    // console.log(results);
 });
--- 10-TypeScript/app.ts
+++ 10-TypeScript/app.ts
@@ -1,6 +1,24 @@
+
+
+
 const num1Input = document.getElementById("num1") as HTMLInputElement;
 const num2Input = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.getElementById("button") as HTMLInputElement;
+
+type PrintMode = "console" | "alert";
+type ResultType = string | number;
+
+enum OutputMode {
+    CONSOLE, ALERT
+};
+
+function printResult(result: ResultType, printMode: OutputMode) {
+    if (printMode === OutputMode.CONSOLE){
+        console.log(result);
+    } else {
+        alert(result);
+    }
+}
 
 function add(a: number, b: number): number {
 	return a + b;
@@ -22,6 +40,7 @@ buttonElement.addEventListener("click", () => {
 	};
 
 	results.push(resultContainer);
-
-	console.log(results);
+    printResult(result, OutputMode.CONSOLE);
+    printResult(result, OutputMode.ALERT);
+	// console.log(results);
 });
