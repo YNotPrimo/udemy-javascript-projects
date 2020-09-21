ecdf07390ea76cbeb4b1f087e450ecee5a1f6ccf
Ivalin Kolev <ivalinkolev0@gmail.com>
2020-09-21 19:49:12 +0300

    Changed code with Vue

--- Vue/01. Goal/app.js
+++ Vue/01. Goal/app.js
@@ -1,13 +1,28 @@
-const btnEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const ulListEl = document.querySelector("ul");
+Vue.createApp({
+	data() {
+		return {
+            goals: [],
+            enteredValue: ""
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = "";
+        }
+    }
+}).mount("#app");
 
-function addGoal(){
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement("li");
-    listItemEl.textContent = enteredValue;
-    ulListEl.appendChild(listItemEl);
-    inputEl.value = "";
-}
+// const btnEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const ulListEl = document.querySelector("ul");
 
-btnEl.addEventListener("click", addGoal);
+// function addGoal(){
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement("li");
+//     listItemEl.textContent = enteredValue;
+//     ulListEl.appendChild(listItemEl);
+//     inputEl.value = "";
+// }
+
+// btnEl.addEventListener("click", addGoal);
